5bdaed8156577889e350ce5997eaa6598a55b6f3
Ervin-Nurhediyanto <cafeinai311@gmail.com>
2020-10-03 23:14:21 +0700

    Update Database

--- Backup Database/blanja-barokah.sql
+++ Backup Database/blanja-barokah.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 14 Sep 2020 pada 11.06
--- Versi server: 10.4.14-MariaDB
--- Versi PHP: 7.4.9
+-- Waktu pembuatan: 03 Okt 2020 pada 18.12
+-- Versi server: 10.4.10-MariaDB
+-- Versi PHP: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -20,30 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `blanja-barokah`
 --
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `address`
---
-
-CREATE TABLE `address` (
-  `id` int(11) NOT NULL,
-  `title` varchar(64) NOT NULL,
-  `name` varchar(64) NOT NULL,
-  `address` text NOT NULL,
-  `city` varchar(64) NOT NULL,
-  `telephoneNumber` varchar(32) NOT NULL,
-  `postalCode` int(11) NOT NULL,
-  `idUser` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `address`
---
-
-INSERT INTO `address` (`id`, `title`, `name`, `address`, `city`, `telephoneNumber`, `postalCode`, `idUser`) VALUES
-(1, 'Home', 'Putriar', 'Jalan Raya Jakarta', 'Jakarta Selatan', '033434236', 52362, 1);
 
 -- --------------------------------------------------------
 
@@ -75,17 +52,18 @@ INSERT INTO `category` (`id`, `nameCategory`) VALUES
 CREATE TABLE `chat` (
   `id` int(11) NOT NULL,
   `chat` varchar(256) NOT NULL,
-  `idProduct` int(11) NOT NULL,
-  `idUser` int(11) NOT NULL
+  `idProduct` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `chat`
 --
 
-INSERT INTO `chat` (`id`, `chat`, `idProduct`, `idUser`) VALUES
-(1, 'haihai', 1, 9),
-(2, 'haiiiihaiiihello', 4, 5);
+INSERT INTO `chat` (`id`, `chat`, `idProduct`) VALUES
+(1, 'haihai', 1),
+(2, 'haiiiihaiiihello', 4),
+(4, 'hello world baru', 1),
+(5, 'haiiiihaiiihello', 1);
 
 -- --------------------------------------------------------
 
@@ -106,7 +84,8 @@ CREATE TABLE `history` (
 --
 
 INSERT INTO `history` (`id`, `idProduct`, `idUser`, `countItem`, `date`) VALUES
-(5, 1, 9, 10, '2020-09-13 09:24:09');
+(1, 2, 1, 150000, '2020-09-11 12:26:03'),
+(3, 3, 1, 200000, '2020-09-11 12:26:49');
 
 -- --------------------------------------------------------
 
@@ -125,25 +104,28 @@ CREATE TABLE `products` (
   `brand` varchar(256) NOT NULL,
   `author` varchar(64) NOT NULL,
   `rate` int(11) NOT NULL,
+  `chat` varchar(256) NOT NULL,
   `condition` varchar(64) NOT NULL,
   `stock` int(11) NOT NULL,
   `description` text NOT NULL,
   `idCategory` int(11) NOT NULL,
-  `idSeller` int(11) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp()
+  `idSeller` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `image`, `price`, `color`, `category`, `size`, `brand`, `author`, `rate`, `condition`, `stock`, `description`, `idCategory`, `idSeller`, `date`) VALUES
-(1, 'T-Shirt Red ', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT0cBnFK2fE8BZV-1hWnwMH_r5NAsAfBrWAgA&usqp=CAU', 1000000, 'gray, purple, blue dark', 'man', 30, 'Pull & Bear', 'Barokah Shope', 10, 'New', 10, 'Lorem ipsum', 1, 2, '2020-09-13 09:35:36'),
-(2, 'T-Shirt Yellow', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT9uhukJG2vhnS27As6cZl8kmzNgJ99g6Q7kg&usqp=CAU', 80000, 'black, blue, red, pink', 'woman', 26, 'Pull & Bear', 'Barokah Shope', 10, 'New', 12, 'Lorem ipsum', 1, 1, '2020-09-13 08:19:51'),
-(3, 'T-Shirt Blue', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQhMiKa2esQ-bAGZjcbzC29Ytb4mNTstHRpaA&usqp=CAU', 70000, 'black, blue, red, pink', 'woman', 26, 'Pull & Bear', 'Barokah Shope', 10, 'New', 12, 'Lorem ipsum', 1, 1, '2020-09-13 08:19:51'),
-(4, 'T-Shirt Gray', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRs48HHkfxrJPJbzBnwySDHTfn5PF_Vt6CNWQ&usqp=CAU', 150000, 'black', 'man', 27, 'Zalora', 'Barokah-Shop', 9, 'New', 23, 'Lorem Ipsum', 2, 1, '2020-09-13 08:19:51'),
-(5, 'T-Shirt Black', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS2mFsTVONDR3yyPs3hiyWXDlgtrhW9CULEwQ&usqp=CAU', 20000, 'black, blue, red, pink', 'woman', 26, 'Pull & Bear', 'Barokah Shope', 10, 'New', 12, 'Lorem ipsum', 1, 1, '2020-09-13 09:33:43'),
-(6, 'T-Shirt White', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS4__ssG4-FRuLtbomtYavtHWLmT_ffoAG0XQ&usqp=CAU', 40000, 'black, blue, red, pink', 'woman', 26, 'Pull & Bear', 'Barokah Shope', 10, 'New', 12, 'Lorem ipsum', 1, 1, '2020-09-14 03:09:57');
+INSERT INTO `products` (`id`, `name`, `image`, `price`, `color`, `category`, `size`, `brand`, `author`, `rate`, `chat`, `condition`, `stock`, `description`, `idCategory`, `idSeller`) VALUES
+(1, 'T-Shirt Red', 'http://localhost:4000/uploads/1600027179128-t-shirt-red.jpg,http://localhost:4000/uploads/1600027179130-t-shirt-yellow.jpg', 95000, 'Red', 'Boys', 24, '', 'Zalora Cloth', 10, '', 'new', 10, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 3),
+(2, 'T-Shirt Black', 'http://localhost:4000/uploads/1599966833380-t-shirt black.jpg', 75000, 'black', 'woman', 26, '', 'Zalora Cloth', 9, '', 'new', 7, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 7),
+(3, 'T-Shirt Grey', 'http://localhost:4000/uploads/1599967527569-t-shirt grey.jpg', 65000, 'Grey', 'Girl', 22, '', 'Zalora Cloth', 7, '', 'new', 3, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 3),
+(4, 'T-Shirt Yellow', 'http://localhost:4000/uploads/1600027257534-t-shirt-yellow.jpg', 80000, 'Yellow', 'Boys', 24, '', 'Zalora Cloth', 7, '', 'new', 6, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 7),
+(5, 'T-Shirt Blue', 'http://localhost:4000/uploads/1600027406855-t-shirt-blue.jpg', 70000, 'Blue', 'Boys', 26, '', 'Zalora Cloth', 9, '', 'new', 7, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 3),
+(6, 'T-Shirt Grey', 'http://localhost:4000/uploads/1599967572468-t-shirt grey.jpg', 65000, 'Grey', 'Girl', 22, '', 'Zalora Cloth', 7, '', 'new', 9, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 7),
+(7, 'T-Shirt Purple', 'http://localhost:4000/uploads/1599967607089-t-shirt purple.jpg', 95000, 'Purple', 'Boys', 24, '', 'Zalora Cloth', 10, '', 'new', 12, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 3),
+(9, 'T-Shirt Blue', 'http://localhost:4000/uploads/1600027431108-t-shirt-blue.jpg', 70000, 'Blue', 'Boys', 26, '', 'Zalora Cloth', 9, '', 'new', 45, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 7),
+(10, 'T-Shirt All', 'http://localhost:4000/uploads/1600027713951-t-shirt-blue.jpg,http://localhost:4000/uploads/1600027713954-t-shirt-grey.jpg,http://localhost:4000/uploads/1600027713956-t-shirt-purple.jpg,http://localhost:4000/uploads/1600027713958-t-shirt-red.jpg', 75000, 'Blue, Red, Purple, Grey', 'Boys', 26, '', 'Zalora Cloth', 10, '', 'new', 17, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 3);
 
 -- --------------------------------------------------------
 
@@ -159,29 +141,28 @@ CREATE TABLE `users` (
   `image` varchar(256) NOT NULL,
   `gender` int(11) NOT NULL,
   `dateOfBirth` date NOT NULL,
-  `phoneNumber` int(11) NOT NULL,
+  `phoneNumber` varchar(64) NOT NULL,
   `storeName` varchar(64) NOT NULL,
   `storeDescription` varchar(256) NOT NULL,
-  `password` varchar(256) NOT NULL
+  `password` varchar(256) NOT NULL,
+  `address` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `users`
 --
 
-INSERT INTO `users` (`id`, `roleId`, `name`, `email`, `image`, `gender`, `dateOfBirth`, `phoneNumber`, `storeName`, `storeDescription`, `password`) VALUES
-(1, 2, 'customer', 'rputria18@gmail.com', '', 0, '0000-00-00', 0, '', '', '$2a$10$W3PEfY4UqCJin2SzYfzxpu1JxItfCcMLvB2znxWS4/eW7L/gc6kHy'),
-(2, 1, 'barokah', 'barokah@gmail.com', '', 0, '0000-00-00', 2147483647, 'Blanja-Barokah', '', '$2a$10$lsIwt5TSsrxbCDFgqcwn8.ETwzo7Af7VzY3rLVrRAKPcCoKdV4l1S');
+INSERT INTO `users` (`id`, `roleId`, `name`, `email`, `image`, `gender`, `dateOfBirth`, `phoneNumber`, `storeName`, `storeDescription`, `password`, `address`) VALUES
+(1, 2, 'putri', 'putri@gmail.com', '', 0, '0000-00-00', '0', '', '', '$2a$10$W3PEfY4UqCJin2SzYfzxpu1JxItfCcMLvB2znxWS4/eW7L/gc6kHy', ''),
+(2, 1, 'barokah', 'barokah@gmail.com', '', 0, '0000-00-00', '2147483647', 'Blanja-Barokah', '', '$2a$10$lsIwt5TSsrxbCDFgqcwn8.ETwzo7Af7VzY3rLVrRAKPcCoKdV4l1S', ''),
+(3, 1, 'Cafein', 'cafeinai311@gmail.com', 'http://localhost:4000/uploads/1601723768668-Cafeinai.jpg', 0, '0000-00-00', '082322059666', 'Store siapa', 'ssevsersg', '$2a$10$afEprfkJJWCwEKbjlj0alODfQB9kZWGDHLxEfu4doyzMPNHzCGt5W', ''),
+(4, 2, 'Im Nayeon', 'cafeinai689@gmail.com', 'http://localhost:4000/uploads/1601722404000-im-nayeon.jpg', 2, '1995-09-10', '082xxxxxx678', '', '', '$2a$10$NEhUEIsjD3E4c.35bi2uheQcHAYMra.xwL2K7.C6XuYPmJKF6C5Aq', ''),
+(6, 2, 'custumeer', 'cafeinai690@gmail.com', '', 0, '0000-00-00', '', '', '', '$2a$10$b0jui0DjUl3xfHVHuZH/uuIo0hxmzHhzmj1yYPfvF0xO7pSO4gEVq', ''),
+(7, 1, 'seller', 'cafeinai312@gmail.com', '', 0, '0000-00-00', '098747474', 'firaun', '', '$2a$10$P7PNgeoaM7guiq621LbvJOwJFQ7ulpbJN.NCcEWAbjWziNJ0eXviC', '');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indeks untuk tabel `address`
---
-ALTER TABLE `address`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `category`
@@ -218,12 +199,6 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT untuk tabel `address`
---
-ALTER TABLE `address`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
 -- AUTO_INCREMENT untuk tabel `category`
 --
 ALTER TABLE `category`
@@ -239,19 +214,19 @@ ALTER TABLE `chat`
 -- AUTO_INCREMENT untuk tabel `history`
 --
 ALTER TABLE `history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
