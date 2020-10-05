c5e627aca45b395755acc15037732675f53071b3
Ervin-Nurhediyanto <cafeinai311@gmail.com>
2020-10-05 19:58:14 +0700

    Update Database History

--- Backup Database/blanja-barokah.sql
+++ Backup Database/blanja-barokah.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 04 Okt 2020 pada 05.15
+-- Waktu pembuatan: 05 Okt 2020 pada 14.55
 -- Versi server: 10.4.10-MariaDB
 -- Versi PHP: 7.3.12
 
@@ -25,6 +25,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `address`
+--
+
+CREATE TABLE `address` (
+  `id` int(11) NOT NULL,
+  `idUser` int(11) NOT NULL,
+  `address` varchar(256) NOT NULL,
+  `setAddress` int(11) NOT NULL,
+  `title` varchar(256) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `city` varchar(64) NOT NULL,
+  `telephoneNumber` varchar(256) NOT NULL,
+  `postalCode` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `address`
+--
+
+INSERT INTO `address` (`id`, `idUser`, `address`, `setAddress`, `title`, `name`, `city`, `telephoneNumber`, `postalCode`) VALUES
+(1, 4, 'JL. Bersama', 1, 'Office address', 'Nayeon', 'Semarang', '089XXXXXX999', '52668'),
+(3, 6, 'JL. Bersama Mantan', 2, 'Mr', 'Cafein', 'Semarang', '089XXXXXX999', '52668'),
+(5, 4, 'Jl. Kemana', 2, 'Home address', 'Jihyo', 'Jogja', '0812342151', '532356');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `category`
 --
 
@@ -41,7 +68,18 @@ INSERT INTO `category` (`id`, `nameCategory`) VALUES
 (1, 'T-Shirt'),
 (2, 'Shorts'),
 (3, 'Jacket'),
-(4, 'Pants');
+(4, 'Pants'),
+(5, 'Wris Watch'),
+(6, 'High Heels'),
+(7, 'Handbag'),
+(8, 'Backbag'),
+(9, 'Socks'),
+(10, 'Glasses'),
+(11, 'Cap'),
+(12, 'Tie'),
+(13, 'Dress'),
+(14, 'Formal Suit'),
+(15, 'Accesories');
 
 -- --------------------------------------------------------
 
@@ -75,17 +113,28 @@ CREATE TABLE `history` (
   `id` int(11) NOT NULL,
   `idProduct` int(11) NOT NULL,
   `idUser` int(11) NOT NULL,
+  `idSeller` int(11) NOT NULL,
   `countItem` int(11) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp()
+  `imageProduct` varchar(256) NOT NULL,
+  `imageTransfer` varchar(256) NOT NULL,
+  `addressUser` varchar(256) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `payment` varchar(64) NOT NULL,
+  `status` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `history`
 --
 
-INSERT INTO `history` (`id`, `idProduct`, `idUser`, `countItem`, `date`) VALUES
-(1, 2, 1, 150000, '2020-09-11 12:26:03'),
-(3, 3, 1, 200000, '2020-09-11 12:26:49');
+INSERT INTO `history` (`id`, `idProduct`, `idUser`, `idSeller`, `countItem`, `imageProduct`, `imageTransfer`, `addressUser`, `date`, `payment`, `status`) VALUES
+(1, 10, 4, 3, 3, '', '', '', '2020-09-11 12:26:03', 'Gopay', 'not yet paid'),
+(3, 11, 4, 3, 1, '', '', '', '2020-09-11 12:26:49', 'Post Indonesia', 'packed'),
+(4, 12, 4, 7, 2, '', '', '', '2020-09-11 12:26:49', 'Mastercard', 'send'),
+(5, 12, 4, 7, 2, '', '', '', '2020-09-11 12:26:49', 'Mastercard', 'completed'),
+(6, 11, 4, 3, 1, '', '', '', '2020-09-11 12:26:49', 'Post Indonesia', 'order cancel'),
+(11, 12, 4, 7, 1, 'http://localhost:4000/uploads/1601772446195-Glasses.jpg', '', '1', '2020-10-05 12:20:31', 'Gopay', 'not yet paid'),
+(12, 11, 4, 3, 2, 'http://localhost:4000/uploads/1601771363270-Backbag-Black.jpg', '', '1', '2020-10-05 12:20:31', 'Gopay', 'not yet paid');
 
 -- --------------------------------------------------------
 
@@ -100,7 +149,7 @@ CREATE TABLE `products` (
   `price` int(11) NOT NULL,
   `color` varchar(64) NOT NULL,
   `category` varchar(64) NOT NULL,
-  `size` int(11) NOT NULL,
+  `size` varchar(16) NOT NULL,
   `brand` varchar(256) NOT NULL,
   `author` varchar(64) NOT NULL,
   `rate` int(11) NOT NULL,
@@ -117,19 +166,19 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `image`, `price`, `color`, `category`, `size`, `brand`, `author`, `rate`, `chat`, `condition`, `stock`, `description`, `idCategory`, `idSeller`) VALUES
-(1, 'T-Shirt Red', 'http://localhost:4000/uploads/1600027179128-t-shirt-red.jpg,http://localhost:4000/uploads/1600027179130-t-shirt-yellow.jpg', 95000, 'Red', 'Boys', 24, '', 'Zalora Cloth', 10, '', 'new', 10, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 3),
-(2, 'T-Shirt Black', 'http://localhost:4000/uploads/1599966833380-t-shirt black.jpg', 75000, 'black', 'woman', 26, '', 'Zalora Cloth', 9, '', 'new', 7, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 7),
-(3, 'T-Shirt Grey', 'http://localhost:4000/uploads/1599967527569-t-shirt grey.jpg', 65000, 'Grey', 'Girl', 22, '', 'Zalora Cloth', 7, '', 'old', 3, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 3),
-(4, 'T-Shirt Yellow', 'http://localhost:4000/uploads/1600027257534-t-shirt-yellow.jpg', 80000, 'Yellow', 'Boys', 24, '', 'Zalora Cloth', 7, '', 'old', 6, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 7),
-(5, 'T-Shirt Blue', 'http://localhost:4000/uploads/1600027406855-t-shirt-blue.jpg', 70000, 'Blue', 'Boys', 26, '', 'Zalora Cloth', 9, '', 'old', 7, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 3),
-(6, 'T-Shirt Grey', 'http://localhost:4000/uploads/1599967572468-t-shirt grey.jpg', 65000, 'Grey', 'Girl', 22, '', 'Zalora Cloth', 7, '', 'old', 9, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 7),
-(7, 'T-Shirt Purple', 'http://localhost:4000/uploads/1599967607089-t-shirt purple.jpg', 95000, 'Purple', 'Boys', 24, '', 'Zalora Cloth', 10, '', 'new', 12, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 3),
-(9, 'T-Shirt Blue', 'http://localhost:4000/uploads/1600027431108-t-shirt-blue.jpg', 70000, 'Blue', 'Boys', 26, '', 'Zalora Cloth', 9, '', 'new', 45, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 7),
-(10, 'T-Shirt All', 'http://localhost:4000/uploads/1600027713951-t-shirt-blue.jpg,http://localhost:4000/uploads/1600027713954-t-shirt-grey.jpg,http://localhost:4000/uploads/1600027713956-t-shirt-purple.jpg,http://localhost:4000/uploads/1600027713958-t-shirt-red.jpg', 75000, 'Blue, Red, Purple, Grey', 'Boys', 26, '', 'Zalora Cloth', 10, '', 'new', 17, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 3),
-(24, 'Black Backbag', '', 350000, '', '', 0, '', '', 0, '', 'new', 3, '<p>Backbag hitang kualitas bagus</p>', 1, 3),
-(25, 'Backbag Black', 'http://localhost:4000/uploads/1601771363270-Backbag-Black.jpg', 350000, '', '', 0, '', '', 0, '', 'new', 3, '<p>Backbag hitang kualitas bagus</p>', 1, 3),
-(26, 'Glasses', 'http://localhost:4000/uploads/1601772446195-Glasses.jpg', 120000, '', '', 0, '', '', 0, '', 'new', 5, '<p>New Glasses</p>', 1, 3),
-(27, 'Topi', 'http://localhost:4000/uploads/1601773007223-Cap.jpg', 35000, '', '', 0, '', '', 0, '', 'new', 7, '<p>Topi baru</p>', 1, 3);
+(1, 'T-Shirt Red', 'http://localhost:4000/uploads/1600027179128-t-shirt-red.jpg,http://localhost:4000/uploads/1600027179130-t-shirt-yellow.jpg', 95000, 'Red', 'Boys', 'XS', '', 'Zalora Cloth', 10, '', 'new', 10, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 3),
+(2, 'T-Shirt Black', 'http://localhost:4000/uploads/1599966833380-t-shirt black.jpg', 75000, 'black', 'woman', 'X', '', 'Zalora Cloth', 9, '', 'new', 7, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 7),
+(3, 'T-Shirt Grey', 'http://localhost:4000/uploads/1599967527569-t-shirt grey.jpg', 65000, 'Grey', 'Girl', 'M', '', 'Zalora Cloth', 7, '', 'old', 3, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 3),
+(4, 'T-Shirt Yellow', 'http://localhost:4000/uploads/1600027257534-t-shirt-yellow.jpg', 80000, 'Yellow', 'Boys', 'L', '', 'Zalora Cloth', 7, '', 'old', 6, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 7),
+(5, 'T-Shirt Blue', 'http://localhost:4000/uploads/1600027406855-t-shirt-blue.jpg', 70000, 'Blue', 'Boys', 'XL', '', 'Zalora Cloth', 9, '', 'old', 7, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 3),
+(6, 'T-Shirt Grey', 'http://localhost:4000/uploads/1599967572468-t-shirt grey.jpg', 65000, 'Grey', 'Girl', 'XL', '', 'Zalora Cloth', 7, '', 'old', 9, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 7),
+(7, 'T-Shirt Purple', 'http://localhost:4000/uploads/1599967607089-t-shirt purple.jpg', 95000, 'Purple', 'Boys', 'M, L, XL', '', 'Zalora Cloth', 10, '', 'new', 12, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 3),
+(8, 'Black Backbag', '', 350000, '', '', '', '', '', 0, '', 'new', 3, '<p>Backbag hitang kualitas bagus</p>', 1, 3),
+(9, 'T-Shirt Blue', 'http://localhost:4000/uploads/1600027431108-t-shirt-blue.jpg', 70000, 'Blue', 'Boys', 'XL', '', 'Zalora Cloth', 9, '', 'new', 45, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 7),
+(10, 'T-Shirt All', 'http://localhost:4000/uploads/1600027713951-t-shirt-blue.jpg,http://localhost:4000/uploads/1600027713954-t-shirt-grey.jpg,http://localhost:4000/uploads/1600027713956-t-shirt-purple.jpg,http://localhost:4000/uploads/1600027713958-t-shirt-red.jpg', 75000, 'Blue, Grey, Purple, Red', 'Boys', 'XS, X, M, L, XL', '', 'Zalora Cloth', 10, '', 'new', 17, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 3),
+(11, 'Backbag Black', 'http://localhost:4000/uploads/1601771363270-Backbag-Black.jpg', 350000, '', '', '', '', '', 0, '', 'new', 3, '<p>Backbag hitang kualitas bagus</p>', 8, 3),
+(12, 'Glasses', 'http://localhost:4000/uploads/1601772446195-Glasses.jpg', 120000, '', '', '', '', '', 0, '', 'new', 5, '<p>New Glasses</p>', 10, 7),
+(13, 'Topi', 'http://localhost:4000/uploads/1601773007223-Cap.jpg', 35000, '', '', '', '', '', 0, '', 'new', 7, '<p>Topi baru</p>', 11, 3);
 
 -- --------------------------------------------------------
 
@@ -148,25 +197,30 @@ CREATE TABLE `users` (
   `phoneNumber` varchar(64) NOT NULL,
   `storeName` varchar(64) NOT NULL,
   `storeDescription` varchar(256) NOT NULL,
-  `password` varchar(256) NOT NULL,
-  `address` varchar(256) NOT NULL
+  `password` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `users`
 --
 
-INSERT INTO `users` (`id`, `roleId`, `name`, `email`, `image`, `gender`, `dateOfBirth`, `phoneNumber`, `storeName`, `storeDescription`, `password`, `address`) VALUES
-(1, 2, 'putri', 'putri@gmail.com', '', 0, '0000-00-00', '0', '', '', '$2a$10$W3PEfY4UqCJin2SzYfzxpu1JxItfCcMLvB2znxWS4/eW7L/gc6kHy', ''),
-(2, 1, 'barokah', 'barokah@gmail.com', '', 0, '0000-00-00', '2147483647', 'Blanja-Barokah', '', '$2a$10$lsIwt5TSsrxbCDFgqcwn8.ETwzo7Af7VzY3rLVrRAKPcCoKdV4l1S', ''),
-(3, 1, 'Cafein', 'cafeinai311@gmail.com', 'http://localhost:4000/uploads/1601742801708-Cafeinai.jpg', 0, '0000-00-00', '082XXXXXX666', 'Barokah Store', 'Toko Online barokah serba berkah', '$2a$10$afEprfkJJWCwEKbjlj0alODfQB9kZWGDHLxEfu4doyzMPNHzCGt5W', ''),
-(4, 2, 'Im Nayeon', 'cafeinai689@gmail.com', 'http://localhost:4000/uploads/1601722404000-im-nayeon.jpg', 2, '1995-09-10', '082xxxxxx678', '', '', '$2a$10$NEhUEIsjD3E4c.35bi2uheQcHAYMra.xwL2K7.C6XuYPmJKF6C5Aq', ''),
-(6, 2, 'custumeer', 'cafeinai690@gmail.com', '', 0, '0000-00-00', '', '', '', '$2a$10$b0jui0DjUl3xfHVHuZH/uuIo0hxmzHhzmj1yYPfvF0xO7pSO4gEVq', ''),
-(7, 1, 'seller', 'cafeinai312@gmail.com', '', 0, '0000-00-00', '098747474', 'firaun', '', '$2a$10$P7PNgeoaM7guiq621LbvJOwJFQ7ulpbJN.NCcEWAbjWziNJ0eXviC', '');
+INSERT INTO `users` (`id`, `roleId`, `name`, `email`, `image`, `gender`, `dateOfBirth`, `phoneNumber`, `storeName`, `storeDescription`, `password`) VALUES
+(1, 2, 'putri', 'putri@gmail.com', '', 0, '0000-00-00', '0', '', '', '$2a$10$W3PEfY4UqCJin2SzYfzxpu1JxItfCcMLvB2znxWS4/eW7L/gc6kHy'),
+(2, 1, 'barokah', 'barokah@gmail.com', '', 0, '0000-00-00', '2147483647', 'Blanja-Barokah', '', '$2a$10$lsIwt5TSsrxbCDFgqcwn8.ETwzo7Af7VzY3rLVrRAKPcCoKdV4l1S'),
+(3, 1, 'Cafein', 'cafeinai311@gmail.com', 'http://localhost:4000/uploads/1601742801708-Cafeinai.jpg', 0, '0000-00-00', '082XXXXXX666', 'Barokah Store', 'Toko Online barokah serba berkah', '$2a$10$afEprfkJJWCwEKbjlj0alODfQB9kZWGDHLxEfu4doyzMPNHzCGt5W'),
+(4, 2, 'Im Nayeon', 'cafeinai689@gmail.com', 'http://localhost:4000/uploads/1601722404000-im-nayeon.jpg', 2, '1995-09-10', '082xxxxxx678', '', '', '$2a$10$NEhUEIsjD3E4c.35bi2uheQcHAYMra.xwL2K7.C6XuYPmJKF6C5Aq'),
+(6, 2, 'custumeer', 'cafeinai690@gmail.com', '', 0, '0000-00-00', '', '', '', '$2a$10$b0jui0DjUl3xfHVHuZH/uuIo0hxmzHhzmj1yYPfvF0xO7pSO4gEVq'),
+(7, 1, 'seller', 'cafeinai312@gmail.com', '', 0, '0000-00-00', '098747474', 'firaun', '', '$2a$10$P7PNgeoaM7guiq621LbvJOwJFQ7ulpbJN.NCcEWAbjWziNJ0eXviC');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `address`
+--
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `category`
@@ -203,10 +257,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `address`
+--
+ALTER TABLE `address`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT untuk tabel `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT untuk tabel `chat`
@@ -218,7 +278,7 @@ ALTER TABLE `chat`
 -- AUTO_INCREMENT untuk tabel `history`
 --
 ALTER TABLE `history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT untuk tabel `products`
