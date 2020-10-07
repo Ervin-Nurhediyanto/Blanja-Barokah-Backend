4a273ea5881bec771f785688f1c0debb040e00a7
Regina Putri <reginaputria2003@gmail.com>
2020-10-07 07:28:25 +0700

    update db

--- Backup Database/blanja-barokah.sql
+++ Backup Database/blanja-barokah.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 07 Okt 2020 pada 01.02
+-- Waktu pembuatan: 07 Okt 2020 pada 02.27
 -- Versi server: 10.4.14-MariaDB
 -- Versi PHP: 7.4.9
 
@@ -99,11 +99,9 @@ CREATE TABLE `chat` (
 
 INSERT INTO `chat` (`id`, `chat`, `idProduct`, `idUser`) VALUES
 (1, 'haihai', 1, 1),
-<<<<<<< HEAD
-(2, 'helloworld', 2, 1);
-=======
-(2, 'haiiiihaiiihello', 4, 2);
->>>>>>> 9158adc418d1f893ffa2534ba1b1877798ae1ac5
+(2, 'haiiiihaiiihello', 4, 2),
+(4, 'hello world baru', 1, 1),
+(5, 'haiiiihaiiihello', 1, 2);
 
 -- --------------------------------------------------------
 
@@ -131,14 +129,14 @@ CREATE TABLE `history` (
 --
 
 INSERT INTO `history` (`id`, `idProduct`, `idUser`, `idSeller`, `nameProduct`, `countItem`, `imageProduct`, `imageTransfer`, `addressUser`, `date`, `payment`, `status`) VALUES
-(1, 10, 4, 3, '', 3, '', '', '', '2020-09-11 12:26:03', 'Gopay', 'not yet paid'),
-(3, 11, 4, 3, '', 1, '', '', '', '2020-09-11 12:26:49', 'Post Indonesia', 'packed'),
-(4, 12, 4, 7, '', 2, '', '', '', '2020-09-11 12:26:49', 'Mastercard', 'send'),
-(5, 12, 4, 7, '', 2, '', '', '', '2020-09-11 12:26:49', 'Mastercard', 'completed'),
-(6, 11, 4, 3, '', 1, '', '', '', '2020-09-11 12:26:49', 'Post Indonesia', 'order cancel'),
-(11, 12, 4, 7, '', 1, 'http://localhost:4000/uploads/1601772446195-Glasses.jpg', '', '1', '2020-10-05 12:20:31', 'Gopay', 'not yet paid'),
-(12, 11, 4, 3, '', 2, 'http://localhost:4000/uploads/1601771363270-Backbag-Black.jpg', '', '1', '2020-10-05 12:20:31', 'Gopay', 'not yet paid'),
-(13, 6, 4, 7, 'T-Shirt Grey', 2, 'http://localhost:4000/uploads/1599967572468-t-shirt grey.jpg', '', '5', '2020-10-05 15:34:07', 'Mastercard', 'not yet paid');
+(1, 10, 4, 3, '', 3, '', '', 1, '2020-09-11 12:26:03', 'Gopay', 'not yet paid'),
+(3, 11, 4, 3, '', 1, '', '', 1, '2020-09-11 12:26:49', 'Post Indonesia', 'packed'),
+(4, 12, 4, 7, '', 2, '', '', 1, '2020-09-11 12:26:49', 'Mastercard', 'send'),
+(5, 12, 4, 7, '', 2, '', '', 1, '2020-09-11 12:26:49', 'Mastercard', 'completed'),
+(6, 11, 4, 3, '', 1, '', '', 1, '2020-09-11 12:26:49', 'Post Indonesia', 'order cancel'),
+(11, 12, 4, 7, '', 1, 'http://localhost:4000/uploads/1601772446195-Glasses.jpg', '', 1, '2020-10-05 12:20:31', 'Gopay', 'not yet paid'),
+(12, 11, 4, 3, '', 2, 'http://localhost:4000/uploads/1601771363270-Backbag-Black.jpg', '', 1, '2020-10-05 12:20:31', 'Gopay', 'not yet paid'),
+(13, 6, 4, 7, 'T-Shirt Grey', 2, 'http://localhost:4000/uploads/1599967572468-t-shirt grey.jpg', '', 5, '2020-10-05 15:34:07', 'Mastercard', 'not yet paid');
 
 -- --------------------------------------------------------
 
@@ -177,7 +175,7 @@ INSERT INTO `products` (`id`, `name`, `image`, `price`, `color`, `category`, `si
 (5, 'T-Shirt Blue', 'http://localhost:4000/uploads/1600027406855-t-shirt-blue.jpg', 70000, 'Blue', 'Boys', 'XL', '', 'Zalora Cloth', 9, '', 'old', 7, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 3),
 (6, 'T-Shirt Grey', 'http://localhost:4000/uploads/1599967572468-t-shirt grey.jpg', 65000, 'Grey', 'Girl', 'XL', '', 'Zalora Cloth', 7, '', 'old', 9, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 7),
 (7, 'T-Shirt Purple', 'http://localhost:4000/uploads/1599967607089-t-shirt purple.jpg', 95000, 'Purple', 'Boys', 'M, L, XL', '', 'Zalora Cloth', 10, '', 'new', 12, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 3),
-(8, 'Black Backbag', 'http://localhost:4000/uploads/1599967607089-t-shirt purple.jpg', 350000, '', '', '', '', '', 0, '', 'new', 3, '<p>Backbag hitang kualitas bagus</p>', 1, 3),
+(8, 'Black Backbag', '', 350000, '', '', '', '', '', 0, '', 'new', 3, '<p>Backbag hitang kualitas bagus</p>', 1, 3),
 (9, 'T-Shirt Blue', 'http://localhost:4000/uploads/1600027431108-t-shirt-blue.jpg', 70000, 'Blue', 'Boys', 'XL', '', 'Zalora Cloth', 9, '', 'new', 45, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 7),
 (10, 'T-Shirt All', 'http://localhost:4000/uploads/1600027713951-t-shirt-blue.jpg,http://localhost:4000/uploads/1600027713954-t-shirt-grey.jpg,http://localhost:4000/uploads/1600027713956-t-shirt-purple.jpg,http://localhost:4000/uploads/1600027713958-t-shirt-red.jpg', 75000, 'Blue, Grey, Purple, Red', 'Boys', 'XS, X, M, L, XL', '', 'Zalora Cloth', 10, '', 'new', 17, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non magna rutrum, pellentesque augue eu, sagittis velit. Phasellus quis laoreet dolor. Fusce nec pharetra quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent sed enim vel turpis blandit imperdiet ac ac felis. Etiam tincidunt tristique placerat.', 1, 3),
 (11, 'Backbag Black', 'http://localhost:4000/uploads/1601771363270-Backbag-Black.jpg', 350000, '', '', '', '', '', 0, '', 'new', 3, '<p>Backbag hitang kualitas bagus</p>', 8, 3),
@@ -214,9 +212,7 @@ INSERT INTO `users` (`id`, `roleId`, `name`, `email`, `image`, `gender`, `dateOf
 (3, 1, 'Cafein', 'cafeinai311@gmail.com', 'http://localhost:4000/uploads/1601742801708-Cafeinai.jpg', 0, '0000-00-00', '082XXXXXX666', 'Barokah Store', 'Toko Online barokah serba berkah', '$2a$10$afEprfkJJWCwEKbjlj0alODfQB9kZWGDHLxEfu4doyzMPNHzCGt5W'),
 (4, 2, 'Im Nayeon', 'cafeinai689@gmail.com', 'http://localhost:4000/uploads/1601722404000-im-nayeon.jpg', 2, '1995-09-10', '082xxxxxx678', '', '', '$2a$10$NEhUEIsjD3E4c.35bi2uheQcHAYMra.xwL2K7.C6XuYPmJKF6C5Aq'),
 (6, 2, 'custumeer', 'cafeinai690@gmail.com', '', 0, '0000-00-00', '', '', '', '$2a$10$b0jui0DjUl3xfHVHuZH/uuIo0hxmzHhzmj1yYPfvF0xO7pSO4gEVq'),
-(7, 1, 'seller', 'cafeinai312@gmail.com', '', 0, '0000-00-00', '098747474', 'firaun', '', '$2a$10$P7PNgeoaM7guiq621LbvJOwJFQ7ulpbJN.NCcEWAbjWziNJ0eXviC'),
-(8, 2, 'Regina Put', 'reginaputri@gmail.com', 'http://localhost:4000/uploads/1602016113620-download.jpg', 2, '2003-01-18', '089538473', '', '', '$2a$10$CvQJBiXsq6TOLfaQoYJq2OqZ24UTTygYrxHgsF4ScogmPnFTpsgja'),
-(9, 1, 'Blanja Barokah', 'blanjabarokah@gmail.com', '', 0, '0000-00-00', '0210384', 'Blanja Barokah', 'Blanja Barokah Jaya', '$2a$10$oZULq4jX9YUJJ9d0ynWLOeeECBwsMylLa03xxfVQzIbnuIwN51uQy');
+(7, 1, 'seller', 'cafeinai312@gmail.com', '', 0, '0000-00-00', '098747474', 'firaun', '', '$2a$10$P7PNgeoaM7guiq621LbvJOwJFQ7ulpbJN.NCcEWAbjWziNJ0eXviC');
 
 --
 -- Indexes for dumped tables
@@ -248,10 +244,10 @@ ALTER TABLE `chat`
 --
 ALTER TABLE `history`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `addressUser` (`addressUser`),
   ADD KEY `idProduct` (`idProduct`),
   ADD KEY `idSeller` (`idSeller`),
-  ADD KEY `idUser` (`idUser`);
+  ADD KEY `idUser` (`idUser`),
+  ADD KEY `addressUser` (`addressUser`);
 
 --
 -- Indeks untuk tabel `products`
@@ -275,13 +271,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT untuk tabel `address`
 --
 ALTER TABLE `address`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT untuk tabel `chat`
@@ -293,19 +289,19 @@ ALTER TABLE `chat`
 -- AUTO_INCREMENT untuk tabel `history`
 --
 ALTER TABLE `history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT untuk tabel `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -328,10 +324,10 @@ ALTER TABLE `chat`
 -- Ketidakleluasaan untuk tabel `history`
 --
 ALTER TABLE `history`
-  ADD CONSTRAINT `history_ibfk_1` FOREIGN KEY (`addressUser`) REFERENCES `address` (`id`),
-  ADD CONSTRAINT `history_ibfk_2` FOREIGN KEY (`idProduct`) REFERENCES `products` (`id`),
-  ADD CONSTRAINT `history_ibfk_3` FOREIGN KEY (`idSeller`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `history_ibfk_4` FOREIGN KEY (`idUser`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `history_ibfk_1` FOREIGN KEY (`idProduct`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `history_ibfk_2` FOREIGN KEY (`idSeller`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `history_ibfk_3` FOREIGN KEY (`idUser`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `history_ibfk_4` FOREIGN KEY (`addressUser`) REFERENCES `address` (`id`);
 
 --
 -- Ketidakleluasaan untuk tabel `products`
